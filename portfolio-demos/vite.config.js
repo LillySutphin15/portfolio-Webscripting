742d8a037f642d72b448c856fbf465d000ff3a4f
Lilly Sutphin <SutphinL16@gmail.com>
2025-03-07 10:34:24 -0500

    added files before zoom meeting just so everythings up to date

--- portfolio-demos/vite.config.js
+++ portfolio-demos/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import React from 'react';
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./", 
-  plugins: [react()],
-});
+    plugins: [react()],
+    base: "/portfolio-demos/",
+    root: "src",
+    build: {
+        outDir: "../docs",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "src/index.html"),
+                Webpage: resolve(__dirname, "src/Webpage/web.html"),
+                Pages: resolve(__dirname, "src/page/page.html"),
+                Todo: resolve(__dirname, "src/toDoApp/toDo.html")
+            },
+        },
+    },
+})
